29edd43a61b47a092de332dca096ff62a8b4384b
SteveISU <97465669+SteveISU@users.noreply.github.com>
2022-09-11 13:38:56 -0500

    Add files via upload

---
+++ fonts/sketch.js
@@ -0,0 +1,17 @@
+let f1, f2, f3;
+
+function setup() {
+  createCanvas(500, 500);
+  f1 = loadFont ("assets/sabotage.ttf");
+  f2 = loadFont ("assets/stencil.ttf");
+  textAlign(CENTER);
+}
+
+function draw() {
+  background("black");
+  fill ("white");
+  textFont (f2, 52);
+text("Pardon \nMy French", width/2, 200);
+textFont(f1, 40);
+text("Dang varmint's", width/2, 300);
+}
---
+++ hello/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(500, 500);
+  textAlign(CENTER) ;
+}
+
+function draw() {
+  background('red') ;
+  fill('purple') ;
+  textSize(32) ;
+  text("Hello World!", 250, 250) ;
+}
