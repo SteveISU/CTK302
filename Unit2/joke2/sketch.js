917d2b683dd0476c4a3ba50c656da1a1aebfe0ff
Steven <smneigh@ilstu.edu>
2022-09-28 17:50:06 -0500

    yep

--- Unit2/joke2/sketch.js
+++ Unit2/joke2/sketch.js
@@ -9,22 +9,22 @@ function setup() {
 function draw() {
   switch (state) {
     case 0:
-    background('white');
+    background('purple');
     text("I invented a new word!", width/2, height/2);
     break;
      
     case 1:
     background('grey');
-    text("Plagiarism!", 250, 250);
-    break;
-  }
-   timmer++;
-   if(timer > 4*60 ){
+    text("Plagiarism!", width/2, height/2);
+    timmer++;
+   if(timer > 2* 60 ){
     timer=0;
-    state++;
+    state = 5;
     if (state > 4 )
       state=0;
     }
+    break;
+  }
    }
 
 
