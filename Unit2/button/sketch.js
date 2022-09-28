2c8b4bbf06cd7fa3fe8ea5e8cfb91ed20db2cd01
Steven <smneigh@ilstu.edu>
2022-09-28 13:48:15 -0500

    Stuff

--- Unit2/button/sketch.js
+++ Unit2/button/sketch.js
@@ -1,35 +1,29 @@
 let state =0;
-timer=0;
 
 function setup() {
   createCanvas(500, 500);
-  //textAlign{CENTER};
+  textAlign(CENTER);
 }
 
 function draw() {
+  
   switch (state) {
     case 0:
     background('white');
     text("Guess \nWhat!", width/2, height/2);
     break;
-     
-    case 1:
+     case 1:
     background('grey');
     text("case 1", 250, 250);
     break;
-  
-    case 2:
-    background('pink');
-    text("case 2", 250, 250);
-    break;
+
   }
-   timmer++;
-   if(timer > 4*60 ){
-    timer=0;
-    state++;
-    if (state>1 ){
-      state=0;
-    }
-   }
+  rect(100,100,100,100);
 }
 
+  function mouseReleased(){
+    if(mouseX>100 && mouseX<200 && mouseY>100 && mouseY < 200){
+    state++;
+    if(state>1)state=0;
+  }
+  }
