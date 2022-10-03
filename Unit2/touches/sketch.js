dfcf3ce33d44abed7e65b42386c4604a8860eb14
Steven <smneigh@ilstu.edu>
2022-10-03 16:53:17 -0500

    aniamls

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -2,6 +2,11 @@ let numberOfTouches ;
 
 function setup() {
   createCanvas(400, 400);
+  i1 = loadImage ("assets/fox.jpeg");
+  i2 = loadImage ("assets/whitefox.jpg");
+  i3 = loadImage ("assets/groundhog.jpeg");
+  i4 = loadImage ("assets/foxagain.jpeg");
+
   imageMode(CENTER);
 }
 
@@ -12,20 +17,24 @@ function draw() {
   
   switch(numberOfTouches) {
     case 0: 
+    image(i1, width/2, height/2 - 10, 595, 567);
       text("no one is touching the screen", 5, 22) ; 
       break ;
       
       case 1: 
+      image(i2, width/2, height/2 - 10, 595, 567);
        text("it's kind of lonely here", 5, 22) ; 
       // put a picture here
       break ;
       
-      case 2:
+      case 2:image
+      image(i3, width/2, height/2 - 10, 595, 567);
       text("two fingers are touching the screen", 5, 22) ; 
             // put a picture here
       break ;
       
       case 3:
+        image(i4, width/2, height/2 - 10, 595, 567);
      text("three are touching the screen", 5, 22) ; 
             // put a picture here
       break ;
