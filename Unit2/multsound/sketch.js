8c2b63eb5cd609eb8647eb933cf5ddd8e758732c
Steven <smneigh@ilstu.edu>
2022-10-03 15:44:10 -0500

    yep

--- Unit2/multsound/sketch.js
+++ Unit2/multsound/sketch.js
@@ -1,5 +1,6 @@
 let state = -1;
 let s1, s2, s3;
+let i1, i2, i3;
 
 function preload(){
   s1= loadSound('assets/allthat.mp3');
@@ -9,6 +10,10 @@ function preload(){
 }
 function setup() {
   createCanvas(800, 800);
+  i1 = loadImage ("assets/jazzstuff.jpg");
+  i2 = loadImage ("assets/citymusic.jpg");
+  i3 = loadImage ("assets/wavemusic.jpg");
+  imageMode(CENTER);
 }
 
 function draw() {
@@ -22,21 +27,27 @@ function draw() {
     break;
 
     case 0:
-      text("0", 100, 100);
+      background('green');
+      image(i1, width/2, height/2 - 10, 595, 567);
+      text("This song is called All That", 100, 100);
       if(!s1.isPlaying()){
         s1.play();
       }
       break;
 
     case 1:
-      text("1", 100, 100);
+      background('orange');
+      image(i2, width/2, height/2 - 10, 595, 567);
+      text("This song is called Creative Minds", 100, 100);
       if(!s2.isPlaying()){
         s2.play();
       }
       break;
 
     case 2:
-      text("2", 100, 100);
+      background('red');
+      image(i3, width/2, height/2 - 10, 595, 567);
+      text("This song is called Dreams", 100, 100);
       if(!s3.isPlaying()){
         s3.play();
       }
