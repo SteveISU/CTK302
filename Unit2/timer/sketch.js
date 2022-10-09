3a9aa655ff7a0bf93cb2361394ba4e9f3d28f06a
Steven <smneigh@ilstu.edu>
2022-10-09 11:40:31 -0500

    o

--- Unit2/timer/sketch.js
+++ Unit2/timer/sketch.js
@@ -1,7 +1,40 @@
+let state = 0;
+let timer = 0;
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
 }
 
 function draw() {
+ // background(220);
 
+  switch (state) {
+    case 0:
+      background("rgb(238,139,31)");
+      timer++;
+  if (timer > 5 * 60) {
+    timer = 0;
+    print(timer = 0);
+    print("beep");
+  }
+text("Hello there!", 100, 100);
+      break;
+
+    case 1:
+      background("rgb(253,46,83)");
+      text("how are you doing", 100, 100);
+      break;
+
+    case 2:
+      background("rgb(50,170,245)");
+      text("today?", 100, 100);
+      break;
+  }
 }
+
+
+ function mouseReleased(){
+    state++;
+   if(state>2){
+     state=0;
+   }
+  }
